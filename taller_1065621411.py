d432cf42066a9524cea8709a169cfb5a49462b2b
Valerie Zequeira <Valerie.zequeira01@usa.edu.co>
2026-03-02 07:53:24 -0500

    Taller 3

--- taller_1065621411.py
+++ taller_1065621411.py
@@ -3,7 +3,15 @@ if edad>18 :
     print("Es mayor de edad")
 else:
         print("Es menor de edad")
-        
+
 #BUCLE
 for i in range(5):
       print("Hola mundo")
+
+#BUCLE WHILE
+contador = 1
+
+while contador <=5:
+      print("numero: " + str(contador))
+      contador = contador + 1
+      
