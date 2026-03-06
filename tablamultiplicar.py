dd23316f992798ef7c1dc8a149b66387c00ae088
valerie Zequeira <valerie.zequeira01@usa.edu.co>
2026-03-06 07:37:46 -0500

    tablas de multiplicar

---
+++ tablamultiplicar.py
@@ -0,0 +1,13 @@
+
+
+print("hola mundo")     # muestra texto en la consola
+x = 42
+print("valor de x:", x) # puedes pasar varias cosas
+
+# en el contexto de las tablas de multiplicar:
+for i in range(1, 11):
+    print(f"Tabla del {i}")
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i*j}")
+    print() # línea en blanco para separar las tablas
+        
